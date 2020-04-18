debe7caf8382305a7960cc4798be9064b8904d53
github88-sys <56423075+github88-sys@users.noreply.github.com>
2020-04-18 22:04:42 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -3,6 +3,7 @@ const Koa = require('koa');
 const app = new Koa();
 app.use((ctx, next) => {
     ctx.response.body = 'hello docker'
+    ctx.response.body = 'hello docker hello git hello ci'
 })
 // 这里是学习测试功能的代码
 // module.exports = function hello(){
